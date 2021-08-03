936f0eb1d20c90a47cce5551fc7cc1c0106b1214
OwczarekGit <null>
2021-08-03 23:50:57 +0200

    Added microphone handler.

---
+++ dwmbard/Handlers/MicrophoneHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using dwmBard.Helpers;
+using dwmBard.Interfaces;
+
+namespace dwmBard.Handlers
+{
+    public class MicrophoneHandler : IParallelWorker, IConfigurable
+    {
+        private string microphoneStatusCommand = "pactl get-source-mute @DEFAULT_SOURCE@";
+        
+        public MicrophoneHandler(int refreshTimeMs) : base(refreshTimeMs)
+        {
+            manualRefreshPossible = true;
+        }
+
+        public override void doWork()
+        {
+            var result = CommandRunner.getCommandOutput(microphoneStatusCommand).Trim();
+
+            if (result.ToLower().Contains("yes"))
+            {
+                returnValuePrefix = "";
+            }
+            else
+            {
+                returnValuePrefix = "";
+            }
+            
+            GC.Collect();
+        }
+    }
+}
