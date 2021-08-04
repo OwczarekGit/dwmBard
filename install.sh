97ff6614b22f6f00325378dcad31246d54df5411
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-08-04 02:44:49 +0200

    Added go back to project directory to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 ./rebuild.sh
 rm -rf ~/.local/share/dwmBard/
 mv dwmBard.tar ~/.local/share/
+curDir="$(pwd)"
 cd ~/.local/share/
 tar -xzvf dwmBard.tar --one-top-level
+cd $curDir
 echo "Installed dwmBard!"
