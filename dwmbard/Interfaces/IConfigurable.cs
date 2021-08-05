34f7187d6208f385c2c77082fb9f747946f37584
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-08-05 16:38:41 +0200

    Prepared IConfigurable interface to be implemented.

--- dwmbard/Interfaces/IConfigurable.cs
+++ dwmbard/Interfaces/IConfigurable.cs
@@ -1,7 +1,13 @@
+using System.Reflection;
+
 namespace dwmBard.Interfaces
 {
     public interface IConfigurable
     {
+        // This will be abstract and each handler that is IConfigurable
+        // will have to implement what can be configured.
+        public abstract void configure();
+
         
     }
 }
