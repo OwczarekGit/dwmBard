c2e772d0bfb79aa174e6ab1294a3d01d86d673db
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-08-05 16:55:23 +0200

    Implemented IConfigurable for WeatherHandler

--- dwmbard/Handlers/WeatherHandler.cs
+++ dwmbard/Handlers/WeatherHandler.cs
@@ -6,23 +6,38 @@ using dwmBard.Interfaces;
 
 namespace dwmBard.Handlers
 {
-    public class WeatherHandler : IParallelWorker
+    public class WeatherHandler : IParallelWorker, IConfigurable
     {
         private string provider = "wttr.in";
         private string location = "Warsaw";
-        private string format = "1";
+        private string format   = "1";
+        private bool tempInF    = false;
         
         public WeatherHandler(int refreshTimeMs) : base(refreshTimeMs){}
 
         public override void doWork()
         {
-            var result = CommandRunner.getCommandOutput($"curl -s \"{provider}/{location}?format={format}").Trim();
+            string result;
+            if (tempInF)
+                result = CommandRunner.getCommandOutput($"curl -s \"{provider}/{location}?format={format}\\&u").Trim();
+            else
+                result = CommandRunner.getCommandOutput($"curl -s \"{provider}/{location}?format={format}").Trim();
+            
             var value = CommandRunner.getCommandOutput($"echo {result} | sed 's/.*[+-]//g'").Trim();
             var isPositive = result.Contains('+');
             var sign = isPositive ? "" : "-";
             
             returnValue = $"  {sign}{value}";
             GC.Collect();
+        }
+
+        public void configure()
+        {
+            var tmpLocation = Program.config.getConfigValue("WeatherHandler.location");
+            location = tmpLocation != null ? tmpLocation : location;
+            
+            var tmpUnit = Program.config.getConfigValue("WeatherHandler.displayinf");
+            tempInF = tmpUnit != null ? bool.Parse(tmpUnit) : tempInF;
         }
     }
 }
