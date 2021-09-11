df6d8e394f67125e1b3630408795b66c0e49cec6
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-09-11 22:07:38 +0200

    Created handler for displaying update count.

---
+++ dwmbard/Daemons/Bar/Handlers/UpdateHandler.cs
@@ -0,0 +1,28 @@
+using System;
+using dwmBard.Helpers;
+using dwmBard.Interfaces;
+
+namespace wmExtender.Daemons.Bar.Handlers
+{
+    public class UpdateHandler : IParallelWorker, IConfigurable
+    {
+        private string updateCountCommand = "pacman -Sup | wc -l";
+        
+        public UpdateHandler(int refreshTimeMs) : base(refreshTimeMs)
+        {
+            manualRefreshPossible = true;
+        }
+
+        public override void doWork()
+        {
+            returnValue = $"  {CommandRunner.getCommandOutput(updateCountCommand).Trim()}";
+            GC.Collect();
+        }
+
+        public void configure()
+        {
+            var tmpCommand = dwmBard.Daemons.Bar.config.getConfigValue("UpdateHandler.updateCountCommand");
+            updateCountCommand = tmpCommand != null ? tmpCommand : updateCountCommand;
+        }
+    }
+}
