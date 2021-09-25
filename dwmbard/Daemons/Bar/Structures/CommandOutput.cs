0bb55802cc4976bdb165871507eff65ae6bbd9f9
OwczarekGit <32412218+OwczarekGit@users.noreply.github.com>
2021-09-25 15:08:40 +0200

    Introduced CommandOutput class that allows for more info on process output.

---
+++ dwmbard/Daemons/Bar/Structures/CommandOutput.cs
@@ -0,0 +1,21 @@
+namespace wmExtender.Daemons.Bar.Structures
+{
+    public class CommandOutput
+    {
+        public string stdOut { get; private set; }
+        public string stdErr { get; private set; }
+        public int exitCode { get; private set; }
+
+        public CommandOutput(string output, string error, int exitCode)
+        {
+            stdOut = output.Trim();
+            stdErr = error.Trim();
+            this.exitCode = exitCode;
+        }
+
+        public string toString()
+        {
+            return $"[{stdOut},{stdErr},{exitCode}]";
+        }
+    }
+}
